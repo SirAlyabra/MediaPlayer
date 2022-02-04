58cf1c4f9ced9afb7f37b9895feee76dacbd65bd
SirAlyabra <alyabra.vargas@gmail.com>
2022-02-04 17:42:35 -0600

    Se agrego el pluggin muted

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,15 @@
 //Media Player 
 function MediaPlayer(config) {
   this.media = config.el
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  })
 }
 
 MediaPlayer.prototype.play = function() {
@@ -19,11 +28,32 @@ MediaPlayer.prototype.togglePlay = function() {
     }
 };
 
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+}
+
+//plugin Autoplay
+function AutoPlay() {
+
+}
+
+AutoPlay.prototype.run = function(player) {
+  player.mute();
+  player.play();
+};
+
+
+//
+
 
 //index.js
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");    
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
 
 button.onclick = () => player.togglePlay();
