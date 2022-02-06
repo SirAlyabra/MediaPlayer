b6fb7ea6739e4fce7acd65b087cc51a0bb90199b
SirAlyabra <alyabra.vargas@gmail.com>
2022-02-06 12:40:31 -0600

    Se implemento el visibility change para deterner el video cuando se cambia de pestaña

--- assets/index.js
+++ assets/index.js
@@ -75,7 +75,9 @@ AutoPlay.prototype.run = function(player) {
 class AutoPause {
   constructor() {
     this.threshold = 0.25;
+
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
@@ -84,14 +86,27 @@ class AutoPause {
     const observer = new IntersectionObserver(this.handleIntersection, {
       threshold:  this.threshold
      });
-     observer.observe(this.player.media)
+     observer.observe(this.player.media);
+
+     document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
+
+
   handleIntersection(entries) {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >  this.threshold
 
     if(isVisible){
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
       this.player.play();
     } else {
       this.player.pause();
