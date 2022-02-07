3273f61047e67b0672336a6a8a0b93cc80d6752d
SirAlyabra <alyabra.vargas@gmail.com>
2022-02-07 08:37:32 -0600

    antes del typeScript

--- sw.js
+++ sw.js
@@ -1,42 +1,45 @@
+const VERSION = 'v1';
+
 self.addEventListener('install', event => {
-    event.waitUntil(precache())
+  event.waitUntil(precache());
 });
 
 self.addEventListener('fetch', event => {
-    const request = event.request;
-    //grt
-    if(request.method != 'GET') {
-        return;
-    }
-    event.respondWidth(cachedResponse(request))
+  const request = event.request;
+  // get
+  if (request.method !== 'GET') {
+    return;
+  }
 
-    //actualizar cache
-    event.waitUntil(updateCache(request));
+  // buscar en cache
+  event.respondWith(cachedResponse(request));
+
+  // actualizar el cache
+  event.waitUntil(updateCache(request));
 });
 
-
-
-async function precache () {
-    const cache = await caches.open("v1")
-    return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/index.js',
-        '/assets/index.js',
-        '/assets/BigBuckBunny.mp4',
-        '/assets/index.css',
-    ]);
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    '/',
+    '/index.html',
+    '/assets/index.js',
+    '/assets/MediaPlayer.js',
+    '/assets/plugins/AutoPlay.js',
+    '/assets/plugins/AutoPause.js',
+    '/assets/index.css',
+    '/assets/BigBuckBunny.mp4',
+  ]);
 }
 
-async function cachedResponse (request) {
-   const cache = await caches.open("v1") 
-   const response = await cache.match(request)
-   return response || fetch(request);
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
 }
 
 async function updateCache(request) {
-    const cache = await caches.open("v1") 
-    const response = await fetch(request)
-    return cache.put(request, request)
-
- }
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
